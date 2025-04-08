7050f695cc58709d8f61967b72862b2baa4c4a87
famati <famati@ufl.edu>
2025-04-08 15:15:34 -0400

    commit to pul

---
+++ cmake-build-debug/CMakeFiles/Medtrics.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Medtrics.dir/Main.cpp.obj"
+  "CMakeFiles/Medtrics.dir/Main.cpp.obj.d"
+  "Medtrics.exe"
+  "Medtrics.exe.manifest"
+  "Medtrics.pdb"
+  "libMedtrics.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Medtrics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
