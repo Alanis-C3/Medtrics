a00303214132c16a03029aa31e6e82b1bd397d11
famati <famati@ufl.edu>
2025-04-17 19:53:49 -0400

    commit merge and heap for state and rating

--- SRC/Heapsort.h
+++ SRC/Heapsort.h
@@ -1,2 +1,47 @@
 // all functions in this class will use heapsort
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
 #pragma once
+using namespace std;
+
+class Heap {
+  public:
+    void stateSort(const map<string, vector<string>> &hospital_data) {
+      auto temp = [](const pair<string, vector<string>> &a, const pair<string, vector<string>> &b) {
+        // state is 4th index of hospita csv
+        return a.second[4] < b.second[4];
+      };
+      // using priority queue for heapsort
+      priority_queue<pair<string, vector<string>>, vector<pair<string, vector<string>>>, decltype(temp)> heap(temp);
+      for (const auto &x : hospital_data) {
+        heap.push(x);
+      }
+      // printing state and hospital name information
+      while (!heap.empty()) {
+        const auto &[name, data] = heap.top();
+        cout << "Hospital: " << name << " | State: " << data[4] << endl;
+        heap.pop();
+      }
+    }
+    void ratingSort(const map<string, vector<string>> &hospital_data) {
+      auto temp = [](const pair<string, vector<string>> &a, const pair<string, vector<string>> &b) {
+        int rowa = a.second[10].empty() ? 0 : stoi(a.second[10]);
+        int rowb = b.second[10].empty() ? 0 : stoi(b.second[10]);
+        // returning higher rating first
+        return rowa < rowb;
+      };
+      // using priority queue for heapsort
+      priority_queue<pair<string, vector<string>>, vector<pair<string, vector<string>>>, decltype(temp)> heap(temp);
+      for (const auto &x : hospital_data) {
+        heap.push(x);
+      }
+      // printing rating and hospital name information
+      while (!heap.empty()) {
+        const auto &[name, data] = heap.top();
+        cout << "Hospital: " << name << " | Rating: " << data[10] << endl;
+        heap.pop();
+      }
+    }
+};
--- SRC/Mergesort.cpp
+++ SRC/Mergesort.cpp
@@ -1,1 +1,79 @@
 // cpp file for all functions using merge sort
+// using logic from discussion slide 12 of module 8 sorting
+#include <iostream>
+#include "Mergesort.h"
+using namespace std;
+// merge state functions:
+// recursive helper function, merge sorting state in alphabetical order
+void Merge::mergeState(vector<pair<string, vector<string>>> &hos, int start, int mid, int end) {
+  vector<pair<string, vector<string>>> temp;
+  int i = start, j = end + 1;
+  while (i <= start && j <= end) {
+    // 4 since state is the 4th index in the csv
+    if (hos[i].second[4] < hos[j].second[4]) {
+      temp.push_back(hos[i++]);
+    }else{
+      temp.push_back(hos[j++]);
+    }
+  }
+  while (i <= start && j <= end) {
+    temp.push_back(hos[i++]);
+    temp.push_back(hos[j++]);
+  }
+  // copying merged information back to temp vector
+  for (int k = i; k <= end; k++) {
+    hos[k] = temp[k-1];
+  }
+}
+// recursive helper for mergeState function
+void Merge::mergesortState(vector<pair<string, vector<string>>> &hos, int start, int end) {
+  if (start < end) {
+    int mid = (start + end) / 2;
+    mergesortState(hos, start, mid);
+    mergesortState(hos, mid + 1, end);
+    mergeState(hos, start, mid, end);
+  }
+}
+// public merge function
+void Merge::stateSort(vector<pair<string, vector<string>>> &hos) {
+  if (!hos.empty()) {
+    mergesortState(hos, 0, hos.size() - 1);
+  }
+}
+// merge rating functions:
+// recursive helper function, merge sorting rating in descending order
+void Merge::mergeRate(vector<pair<string, vector<string>>> &hos, int start, int mid, int end) {
+  vector<pair<string, vector<string>>> temp;
+  int i = start, j = mid + 1;
+  while (i <= mid && j <= end) {
+    int rowi = hos[i].second[10].empty() ? 0 : stoi(hos[i].second[10]);
+    int rowj = hos[j].second[10].empty() ? 0 : stoi(hos[j].second[10]);
+    if (rowi > rowj) {
+      temp.push_back(hos[i++]);
+    }else{
+      temp.push_back(hos[j++]);
+    }
+  }
+  while (i <= mid && j <= end) {
+    temp.push_back(hos[i++]);
+    temp.push_back(hos[j++]);
+  }
+  for (int k = i; k <= end; k++) {
+    hos[k] = temp[k-1];
+  }
+}
+// recursive helper for ratingSort
+void Merge::mergesortRate(vector<pair<string, vector<string>>> &hos, int start, int end) {
+  if (start < end) {
+    int mid = (start + end) / 2;
+    mergesortRate(hos, start, mid);
+    mergesortRate(hos, mid + 1, end);
+    mergeRate(hos, start, mid, end);
+  }
+}
+// public ratingSort function
+void Merge::ratingSort(vector<pair<string, vector<string>>> &hos) {
+  if (!hos.empty()) {
+    mergesortRate(hos, 0, hos.size() - 1);
+  }
+}
